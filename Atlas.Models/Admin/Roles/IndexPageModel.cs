bd1554e433846939529de30599b191a0a20b19dd
Luca Briguglia <luca.briguglia@outlook.com>
2020-07-30 17:28:53 +0100

    Various updates

---
+++ Atlas.Models/Admin/Roles/IndexPageModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Atlas.Models.Admin.Roles
+{
+    public class IndexPageModel
+    {
+        public IList<RoleModel> Roles { get; set; } = new List<RoleModel>();
+
+        public EditRoleModel EditRole { get; set; } = new EditRoleModel();
+
+        public class EditRoleModel
+        {
+            public string Id { get; set; }
+
+            [Required]
+            public string Name { get; set; }
+        }
+
+        public class RoleModel
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
