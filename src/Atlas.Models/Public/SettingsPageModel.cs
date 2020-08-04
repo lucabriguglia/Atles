6beb91bc4d633e651ae7b0544953074321baefb9
Luca Briguglia <luca.briguglia@outlook.com>
2020-08-04 14:10:58 +0100

    Various updates

---
+++ src/Atlas.Models/Public/SettingsPageModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Atlas.Models.Public
+{
+    public class SettingsPageModel
+    {
+        public MemberModel Member { get; set; } = new MemberModel();
+
+        public class MemberModel
+        {
+            public Guid Id { get; set; }
+            public string Email { get; set; }
+            public string DisplayName { get; set; }
+            public string GravatarHash { get; set; }
+        }
+    }
+}
---
+++ src/Atlas.Server/Controllers/Public/SettingsController.cs
@@ -0,0 +1,75 @@
+﻿using System.Threading.Tasks;
+using Atlas.Domain.Members;
+using Atlas.Domain.Members.Commands;
+using Atlas.Models.Public;
+using Atlas.Server.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Atlas.Server.Controllers.Public
+{
+    [Authorize]
+    [Route("api/public/settings")]
+    [ApiController]
+    public class SettingsController : ControllerBase
+    {
+        private readonly IContextService _contextService;
+        private readonly IPublicModelBuilder _modelBuilder;
+        private readonly IMemberService _memberService;
+        private readonly IMemberRules _memberRules;
+
+        public SettingsController(IContextService contextService, 
+            IPublicModelBuilder modelBuilder,
+            IMemberService memberService, 
+            IMemberRules memberRules)
+        {
+            _contextService = contextService;
+            _modelBuilder = modelBuilder;
+            _memberService = memberService;
+            _memberRules = memberRules;
+        }
+
+        [Authorize]
+        [HttpGet("edit")]
+        public async Task<SettingsPageModel> Edit()
+        {
+            var member = await _contextService.CurrentMemberAsync();
+
+            var model = await _modelBuilder.BuildSettingsPageModelAsync(member.Id);
+
+            return model;
+        }
+
+        [Authorize]
+        [HttpPost("update")]
+        public async Task<ActionResult> Update(SettingsPageModel model)
+        {
+            var site = await _contextService.CurrentSiteAsync();
+            var member = await _contextService.CurrentMemberAsync();
+
+            if (model.Member.Id != member.Id)
+            {
+                return Unauthorized();
+            }
+
+            var command = new UpdateMember
+            {
+                Id = member.Id,
+                DisplayName = model.Member.DisplayName,
+                SiteId = site.Id,
+                MemberId = member.Id
+            };
+
+            await _memberService.UpdateAsync(command);
+
+            return Ok();
+        }
+
+        [HttpGet("is-display-name-unique/{name}")]
+        public async Task<IActionResult> IsDisplayNameUnique(string name)
+        {
+            var isNameUnique = await _memberRules.IsDisplayNameUniqueAsync(name);
+            return Ok(isNameUnique);
+        }
+    }
+}
