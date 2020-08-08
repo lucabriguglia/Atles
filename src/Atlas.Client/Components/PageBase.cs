f4382685437b579e80a9cd18f9631ef1b15e2d3f
Luca Briguglia <luca.briguglia@outlook.com>
2020-08-08 14:49:24 +0100

    Various updates

---
+++ src/Atlas.Client/Components/PageBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace Atlas.Client.Components
+{
+    public abstract class PageBase : ComponentBase
+    {
+        protected RenderFragment AddComponent(string component, object model) => builder =>
+        {
+            const string theme = "Classic";
+
+            var type = Type.GetType($"Atlas.Client.Themes.{theme}.{component}, {typeof(Program).Assembly.FullName}");
+
+            builder.OpenComponent(0, type);
+            builder.AddAttribute(1, "Model", model);
+            builder.CloseComponent();
+        };
+    }
+}
---
+++ src/Atlas.Client/Components/Themes/MemberComponentBase.cs
@@ -0,0 +1,11 @@
+﻿using Atlas.Models.Public;
+using Microsoft.AspNetCore.Components;
+
+namespace Atlas.Client.Components.Themes
+{
+    public class MemberComponentBase : ComponentBase
+    {
+        [Parameter]
+        public MemberPageModel Model { get; set; }
+    }
+}
