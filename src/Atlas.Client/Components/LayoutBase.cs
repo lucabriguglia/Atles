6fb99ff4719086458c94fd98b641658e852fb689
Luca Briguglia <luca.briguglia@outlook.com>
2020-08-08 17:49:09 +0100

    Add layout base

---
+++ src/Atlas.Client/Components/LayoutBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Atlas.Client.Services;
+using Atlas.Models.Public;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Atlas.Client.Components
+{
+    public abstract class LayoutBase : LayoutComponentBase
+    {
+        [Inject] public IJSRuntime JsRuntime { get; set; }
+        [Inject] public ApiService ApiService { get; set; }
+
+        protected CurrentSiteModel Site { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Site = await ApiService.GetFromJsonAsync<CurrentSiteModel>("api/public/current-site");
+            await JsRuntime.InvokeVoidAsync("changePageTitle", Site.Title);
+            await JsRuntime.InvokeVoidAsync("addCssFile", "public");
+        }
+
+        protected RenderFragment AddLayout(string layout) => builder =>
+        {
+            var type = Type.GetType($"Atlas.Client.Themes.{Site.Theme}.{layout}, {typeof(Program).Assembly.FullName}");
+
+            builder.OpenComponent(0, type);
+            builder.CloseComponent();
+        };
+    }
+}
---
+++ src/Atlas.Client/Components/Themes/ThemeLayoutComponentBase.cs
@@ -0,0 +1,11 @@
+﻿using Atlas.Models.Public;
+using Microsoft.AspNetCore.Components;
+
+namespace Atlas.Client.Components.Themes
+{
+    public abstract class ThemeLayoutComponentBase : LayoutComponentBase
+    {
+        [CascadingParameter]
+        protected CurrentSiteModel Site { get; set; }
+    }
+}
