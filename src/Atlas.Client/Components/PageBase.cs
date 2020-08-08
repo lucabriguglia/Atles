05f905ad63b66931aa2496587113180f8273c1db
Luca Briguglia <lubrigug@microsoft.com>
2020-08-08 22:08:23 +0100

    Various updates

--- src/Atlas.Client/Components/PageBase.cs
+++ src/Atlas.Client/Components/PageBase.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Atlas.Models.Public;
 using Microsoft.AspNetCore.Components;
 
@@ -9,12 +11,21 @@ namespace Atlas.Client.Components
         [CascadingParameter] 
         protected CurrentSiteModel Site { get; set; }
 
-        protected RenderFragment AddComponent(string component, object model) => builder =>
+        protected RenderFragment AddComponent(string component, Dictionary<string, object> models = null) => builder =>
         {
             var type = Type.GetType($"Atlas.Client.Themes.{Site.Theme}.{component}, {typeof(Program).Assembly.FullName}");
 
             builder.OpenComponent(0, type);
-            builder.AddAttribute(1, "Model", model);
+
+            if (models != null)
+            {
+                for (int i = 0; i < models.Count; i++)
+                {
+                    var item = models.ElementAt(i);
+                    builder.AddAttribute(i + 1, item.Key, item.Value);
+                }
+            }
+            
             builder.CloseComponent();
         };
     }
---
+++ src/Atlas.Client/Components/Themes/IndexComponentBase.cs
@@ -0,0 +1,11 @@
+﻿using Atlas.Models.Public;
+using Microsoft.AspNetCore.Components;
+
+namespace Atlas.Client.Components.Themes
+{
+    public class IndexComponentBase : ThemeComponentBase
+    {
+        [Parameter]
+        public IndexPageModel Model { get; set; }
+    }
+}
