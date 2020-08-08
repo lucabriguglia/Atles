caa97e1e787f36ad94bad6b64e2ea6df22afa1a7
Luca Briguglia <lubrigug@microsoft.com>
2020-08-08 22:56:22 +0100

    Add forum component

---
+++ src/Atlas.Client/Components/Themes/ForumComponentBase.cs
@@ -0,0 +1,66 @@
+﻿using Atlas.Client.Services;
+using Atlas.Models;
+using Atlas.Models.Public;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+
+namespace Atlas.Client.Components.Themes
+{
+    public class ForumComponentBase : ThemeComponentBase
+    {
+        [Inject]
+        public ApiService ApiService { get; set; }
+
+        [Inject]
+        public IJSRuntime JsRuntime { get; set; }
+
+        [Parameter]
+        public ForumPageModel Model { get; set; }
+
+        [Parameter]
+        public Guid Id { get; set; }
+
+        public int CurrentPage { get; set; } = 1;
+        public string Search { get; set; }
+
+        protected async Task SearchAsync()
+        {
+            CurrentPage = 1;
+            await LoadTopicsAsync();
+        }
+
+        protected async Task ClearSearchAsync()
+        {
+            if (!string.IsNullOrWhiteSpace(Search))
+            {
+                Search = string.Empty;
+                CurrentPage = 1;
+                await LoadTopicsAsync();
+            }
+        }
+
+        protected async Task MyKeyUpAsync(KeyboardEventArgs key)
+        {
+            if (key.Code == "Enter")
+            {
+                await SearchAsync();
+            }
+        }
+
+        protected async Task ChangePageAsync(int page)
+        {
+            await JsRuntime.InvokeVoidAsync("scrollToTarget", "topics");
+            CurrentPage = page;
+            await LoadTopicsAsync();
+        }
+
+        private async Task LoadTopicsAsync()
+        {
+            Model.Topics = null;
+            Model.Topics = await ApiService.GetFromJsonAsync<PaginatedData<ForumPageModel.TopicModel>>($"api/public/forums/{Id}/topics?page={CurrentPage}&search={Search}");
+        }
+    }
+}
