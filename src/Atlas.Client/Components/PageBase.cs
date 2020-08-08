086280ab7e46d0d4e890519a586d454b92cc634e
Luca Briguglia <luca.briguglia@outlook.com>
2020-08-08 17:29:59 +0100

    Add site cascading parameter

--- src/Atlas.Client/Components/PageBase.cs
+++ src/Atlas.Client/Components/PageBase.cs
@@ -1,15 +1,17 @@
 ﻿using System;
+using Atlas.Models.Public;
 using Microsoft.AspNetCore.Components;
 
 namespace Atlas.Client.Components
 {
     public abstract class PageBase : ComponentBase
     {
+        [CascadingParameter] 
+        protected CurrentSiteModel Site { get; set; }
+
         protected RenderFragment AddComponent(string component, object model) => builder =>
         {
-            const string theme = "Classic";
-
-            var type = Type.GetType($"Atlas.Client.Themes.{theme}.{component}, {typeof(Program).Assembly.FullName}");
+            var type = Type.GetType($"Atlas.Client.Themes.{Site.Theme}.{component}, {typeof(Program).Assembly.FullName}");
 
             builder.OpenComponent(0, type);
             builder.AddAttribute(1, "Model", model);
