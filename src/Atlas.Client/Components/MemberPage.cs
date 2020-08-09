8eb4ec469c0d1726eff1d3b01c57ad83338a9312
Luca Briguglia <luca.briguglia@outlook.com>
2020-08-09 09:29:55 +0100

    Various updates

---
+++ src/Atlas.Client/Components/MemberPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Atlas.Models.Public;
+using Microsoft.AspNetCore.Components;
+
+namespace Atlas.Client.Components
+{
+    public abstract class MemberPage : PageBase
+    {
+        [Parameter]
+        public Guid Id { get; set; }
+
+        protected MemberPageModel Model { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Model = await ApiService.GetFromJsonAsync<MemberPageModel>($"api/public/members/{Id}");
+        }
+    }
+}
---
+++ src/Atlas.Client/Components/ProfilePage.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Atlas.Models.Public;
+
+namespace Atlas.Client.Components
+{
+    public abstract class ProfilePage : PageBase
+    {
+        protected MemberPageModel Model { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Model = await ApiService.GetFromJsonAsync<MemberPageModel>("api/public/members");
+        }
+    }
+}
