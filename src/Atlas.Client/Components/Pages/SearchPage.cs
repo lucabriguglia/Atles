789f24eb06aaf646bd1b6549481eb1680e8896c6
Luca Briguglia <luca.briguglia@outlook.com>
2020-08-09 19:30:04 +0100

    Add search page and component

---
+++ src/Atlas.Client/Components/Pages/SearchPage.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Atlas.Models.Public;
+
+namespace Atlas.Client.Components.Pages
+{
+    public abstract class SearchPage : PageBase
+    {
+        public SearchPageModel Model { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Model = await ApiService.GetFromJsonAsync<SearchPageModel>("api/public/search?page=1");
+        }
+    }
+}
---
+++ src/Atlas.Client/Components/Themes/SearchComponentBase.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading.Tasks;
+using Atlas.Models.Public;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
+
+namespace Atlas.Client.Components.Themes
+{
+    public class SearchComponentBase : ThemeComponentBase
+    {
+        [Parameter] public SearchPageModel Model { get; set; }
+
+        protected string SearchTerm { get; set; }
+        protected int CurrentPage { get; set; } = 1;
+
+        protected async Task MyKeyUpAsync(KeyboardEventArgs key)
+        {
+            if (key.Code == "Enter")
+            {
+                await SearchAsync();
+            }
+        }
+
+        protected async Task SearchAsync()
+        {
+            Model.Posts = null;
+            StateHasChanged();
+            await LoadDataAsync();
+        }
+
+        protected async Task ClearSearchAsync()
+        {
+            if (!string.IsNullOrWhiteSpace(SearchTerm))
+            {
+                SearchTerm = string.Empty;
+                Model.Posts = null;
+                StateHasChanged();
+                await LoadDataAsync();
+            }
+        }
+
+        protected async Task ChangePageAsync(int page)
+        {
+            await JsRuntime.InvokeVoidAsync("scrollToTarget", "posts");
+            CurrentPage = page;
+            Model.Posts = null;
+            StateHasChanged();
+            await LoadDataAsync();
+        }
+
+        private async Task LoadDataAsync()
+        {
+            Model = await ApiService.GetFromJsonAsync<SearchPageModel>($"api/public/search?page={CurrentPage}&search={SearchTerm}");
+        }
+    }
+}
