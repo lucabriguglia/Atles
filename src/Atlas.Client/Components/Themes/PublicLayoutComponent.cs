5d071f1e018425127cd90c500949e705f9bd2704
Luca Briguglia <luca.briguglia@outlook.com>
2020-08-13 07:48:54 +0100

    Various updates

--- src/Atlas.Client/Components/Themes/PublicLayoutComponent.cs
+++ src/Atlas.Client/Components/Themes/PublicLayoutComponent.cs
@@ -1,6 +1,8 @@
 ﻿using System.Threading.Tasks;
+using Atlas.Client.Pages;
 using Atlas.Models.Public;
 using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
 using Microsoft.JSInterop;
 
 namespace Atlas.Client.Components.Themes
@@ -8,6 +10,7 @@ namespace Atlas.Client.Components.Themes
     public abstract class PublicLayoutComponent : LayoutComponentBase
     {
         [Inject] public IJSRuntime JsRuntime { get; set; }
+        [Inject] public IStringLocalizer<PublicResources> Loc { get; set; }
 
         [CascadingParameter] protected CurrentMemberModel Member { get; set; }
         [CascadingParameter] protected CurrentSiteModel Site { get; set; }
