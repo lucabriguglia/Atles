d8ab81c0454a6419d25b0f82fdd3f5b34c3da3da
Luca Briguglia <luca.briguglia@outlook.com>
2020-08-14 10:45:56 +0100

    Various updates

--- src/Atlas.Client/Components/Pages/IndexPage.cs
+++ src/Atlas.Client/Components/Pages/IndexPage.cs
@@ -1,16 +1,6 @@
-﻿using System.Threading.Tasks;
-
-namespace Atlas.Client.Components.Pages
+﻿namespace Atlas.Client.Components.Pages
 {
     public abstract class IndexPage : PageBase
     {
-        public bool DisplayForums { get; set; }
-
-        protected override async Task OnInitializedAsync()
-        {
-            await base.OnInitializedAsync();
-
-            DisplayForums = true;
-        }
     }
 }
