687c8c86a89a7f96d1294782c3bd2845989401c5
Luca Briguglia <luca.briguglia@outlook.com>
2020-08-20 10:21:28 +0100

    Add post menu component

---
+++ src/Atlas.Client/Components/Shared/PostMenuComponent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Atlas.Client.Components.Shared
+{
+    public abstract class PostMenuComponent : SharedComponentBase
+    {
+        [Parameter] public Guid PostId { get; set; }
+
+        [Parameter] public bool CanAcceptAnswer { get; set; }
+        [Parameter] public bool CanRemoveAnswer { get; set; }
+        [Parameter] public bool CanEdit { get; set; }
+        [Parameter] public bool CanDelete { get; set; }
+
+        [Parameter] public EventCallback<MouseEventArgs> AcceptAnswerCallback { get; set; }
+        [Parameter] public EventCallback<MouseEventArgs> RemoveAnswerCallback { get; set; }
+        [Parameter] public EventCallback<MouseEventArgs> EditCallback { get; set; }
+        [Parameter] public EventCallback<MouseEventArgs> DeleteCallback { get; set; }
+
+        protected bool Display { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Display = CanAcceptAnswer || CanRemoveAnswer || CanEdit || CanDelete;
+        }
+    }
+}
