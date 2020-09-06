4e21c6a1b95911d89191f27f034e804e13bd4306
Luca Briguglia <luca.briguglia@outlook.com>
2020-09-06 16:27:05 +0100

    Update Docs.razor and documentation.v1.json

--- src/Atlas.Client/Components/Admin/DocsPage.cs
+++ src/Atlas.Client/Components/Admin/DocsPage.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Docs.Models;
 
 namespace Atlas.Client.Components.Admin
@@ -7,9 +8,18 @@ namespace Atlas.Client.Components.Admin
     {
         protected DocumentationModel Model { get; set; }
 
+        public TargetModel SelectedTarget { get; set; }
+
         protected override async Task OnInitializedAsync()
         {
             Model = await ApiService.GetFromJsonAsync<DocumentationModel>("api/admin/documentation");
+            SelectedTarget = Model.Contexts[0].Targets[0];
+        }
+
+        protected void SelectTarget(string contextName, string targetName)
+        {
+            var selectedContext = Model.Contexts.FirstOrDefault(x => x.Name == contextName);
+            SelectedTarget = selectedContext?.Targets.FirstOrDefault(x => x.Name == targetName);
         }
     }
 }
