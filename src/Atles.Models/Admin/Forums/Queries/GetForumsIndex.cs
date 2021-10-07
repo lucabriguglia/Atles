b63636577f066e641eba0c2e2311c6db22638610
Luca Briguglia <luca.briguglia@outlook.com>
2021-10-07 08:23:28 +0100

    Add get forum index query and handler

---
+++ src/Atles.Models/Admin/Forums/Queries/GetForumsIndex.cs
@@ -0,0 +1,11 @@
+﻿using Atles.Infrastructure.Queries;
+using Atles.Models.Admin.Forums;
+using System;
+
+namespace Atles.Reporting.Admin.Forums
+{
+    public class GetForumsIndex : QueryBase<IndexPageModel>
+    {
+        public Guid? CategoryId { get; set; }
+    }
+}
