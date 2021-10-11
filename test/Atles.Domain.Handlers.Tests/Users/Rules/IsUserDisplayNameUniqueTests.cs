47ca0831bab484f0973380b19084ddc0902a4e83
Luca Briguglia <luca.briguglia@outlook.com>
2021-10-11 13:24:52 +0100

    Fix unit tests

--- test/Atles.Domain.Handlers.Tests/Users/Rules/IsUserDisplayNameUniqueTests.cs
+++ test/Atles.Domain.Handlers.Tests/Users/Rules/IsUserDisplayNameUniqueTests.cs
@@ -12,26 +12,44 @@ namespace Atles.Domain.Handlers.Tests.Users.Rules
     public class IsUserDisplayNameUniqueTests : TestFixtureBase
     {
         [Test]
-        [Ignore("")]
         public async Task Should_return_true_when_display_name_is_unique()
         {
-            using (var dbContext = new AtlesDbContext(Shared.CreateContextOptions()))
+            var options = Shared.CreateContextOptions();
+
+            using (var dbContext = new AtlesDbContext(options))
+            {
+                var user = new User(Guid.NewGuid().ToString(), "me@email.com", "Display Name");
+                dbContext.Users.Add(user);
+                await dbContext.SaveChangesAsync();
+            }
+
+            using (var dbContext = new AtlesDbContext(options))
             {
                 var sut = new IsUserDisplayNameUniqueHandler(dbContext);
-                var actual = await sut.Handle(new IsUserDisplayNameUnique { DisplayName = "Display Name" });
+                var actual = await sut.Handle(new IsUserDisplayNameUnique { DisplayName = "Blah blah" });
 
                 Assert.IsTrue(actual);
             }
         }
 
         [Test]
-        [Ignore("")]
         public async Task Should_return_true_when_display_name_is_unique_for_existing_member()
         {
-            using (var dbContext = new AtlesDbContext(Shared.CreateContextOptions()))
+            var options = Shared.CreateContextOptions();
+
+            using (var dbContext = new AtlesDbContext(options))
+            {
+                var user1 = new User(Guid.NewGuid().ToString(), "me1@email.com", "User 1");
+                var user2 = new User(Guid.NewGuid().ToString(), "me2@email.com", "User 2");
+                dbContext.Users.Add(user1);
+                dbContext.Users.Add(user2);
+                await dbContext.SaveChangesAsync();
+            }
+
+            using (var dbContext = new AtlesDbContext(options))
             {
                 var sut = new IsUserDisplayNameUniqueHandler(dbContext);
-                var actual = await sut.Handle(new IsUserDisplayNameUnique { DisplayName = "Display Name", Id = Guid.NewGuid() });
+                var actual = await sut.Handle(new IsUserDisplayNameUnique { DisplayName = "User 3", Id = Guid.NewGuid() });
 
                 Assert.IsTrue(actual);
             }
