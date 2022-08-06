4c5f04b3595f06b0f9c75cb3177bf46e18de0f2f
lucabriguglia <luca.briguglia@outlook.com>
2022-08-06 10:08:21 +0100

    Remove validators from forum commands handlers

--- src/Atles.Commands.Handlers/Forums/MoveForumHandler.cs
+++ src/Atles.Commands.Handlers/Forums/MoveForumHandler.cs
@@ -10,86 +10,85 @@ using Atles.Domain;
 using Atles.Events.Forums;
 using Microsoft.EntityFrameworkCore;
 
-namespace Atles.Commands.Handlers.Forums
+namespace Atles.Commands.Handlers.Forums;
+
+public class MoveForumHandler : ICommandHandler<MoveForum>
 {
-    public class MoveForumHandler : ICommandHandler<MoveForum>
+    private readonly AtlesDbContext _dbContext;
+    private readonly ICacheManager _cacheManager;
+
+    public MoveForumHandler(AtlesDbContext dbContext,
+        ICacheManager cacheManager)
     {
-        private readonly AtlesDbContext _dbContext;
-        private readonly ICacheManager _cacheManager;
+        _dbContext = dbContext;
+        _cacheManager = cacheManager;
+    }
 
-        public MoveForumHandler(AtlesDbContext dbContext,
-            ICacheManager cacheManager)
+    public async Task<CommandResult> Handle(MoveForum command)
+    {
+        var forum = await _dbContext.Forums
+            .FirstOrDefaultAsync(x =>
+                x.Category.SiteId == command.SiteId &&
+                x.Id == command.ForumId &&
+                x.Status != ForumStatusType.Deleted);
+
+        if (forum == null)
         {
-            _dbContext = dbContext;
-            _cacheManager = cacheManager;
+            throw new DataException($"Forum with Id {command.ForumId} not found.");
         }
 
-        public async Task<CommandResult> Handle(MoveForum command)
+        if (command.Direction == DirectionType.Up)
         {
-            var forum = await _dbContext.Forums
-                .FirstOrDefaultAsync(x =>
-                    x.Category.SiteId == command.SiteId &&
-                    x.Id == command.ForumId &&
-                    x.Status != ForumStatusType.Deleted);
-
-            if (forum == null)
-            {
-                throw new DataException($"Forum with Id {command.ForumId} not found.");
-            }
-
-            if (command.Direction == DirectionType.Up)
-            {
-                forum.MoveUp();
-            }
-            else if (command.Direction == DirectionType.Down)
-            {
-                forum.MoveDown();
-            }
-
-            var @event = new ForumMoved
-            {
-                SortOrder = forum.SortOrder,
-                TargetId = forum.Id,
-                TargetType = nameof(Forum),
-                SiteId = command.SiteId,
-                UserId = command.UserId
-            };
-
-            _dbContext.Events.Add(@event.ToDbEntity());
-
-            var sortOrderToReplace = forum.SortOrder;
-
-            var adjacentForum = await _dbContext.Forums
-                .FirstOrDefaultAsync(x =>
-                    x.CategoryId == forum.CategoryId &&
-                    x.SortOrder == sortOrderToReplace &&
-                    x.Status != ForumStatusType.Deleted);
-
-            if (command.Direction == DirectionType.Up)
-            {
-                adjacentForum.MoveDown();
-            }
-            else if (command.Direction == DirectionType.Down)
-            {
-                adjacentForum.MoveUp();
-            }
-
-            var adjacentForumMoved = new ForumMoved
-            {
-                SortOrder = adjacentForum.SortOrder,
-                TargetId = adjacentForum.Id,
-                TargetType = nameof(Forum),
-                SiteId = command.SiteId,
-                UserId = command.UserId
-            };
-
-            _dbContext.Events.Add(adjacentForumMoved.ToDbEntity());
-
-            await _dbContext.SaveChangesAsync();
-
-            _cacheManager.Remove(CacheKeys.Categories(command.SiteId));
-
-            return new Success(new IEvent[] { @event });
+            forum.MoveUp();
         }
+        else if (command.Direction == DirectionType.Down)
+        {
+            forum.MoveDown();
+        }
+
+        var @event = new ForumMoved
+        {
+            SortOrder = forum.SortOrder,
+            TargetId = forum.Id,
+            TargetType = nameof(Forum),
+            SiteId = command.SiteId,
+            UserId = command.UserId
+        };
+
+        _dbContext.Events.Add(@event.ToDbEntity());
+
+        var sortOrderToReplace = forum.SortOrder;
+
+        var adjacentForum = await _dbContext.Forums
+            .FirstOrDefaultAsync(x =>
+                x.CategoryId == forum.CategoryId &&
+                x.SortOrder == sortOrderToReplace &&
+                x.Status != ForumStatusType.Deleted);
+
+        if (command.Direction == DirectionType.Up)
+        {
+            adjacentForum.MoveDown();
+        }
+        else if (command.Direction == DirectionType.Down)
+        {
+            adjacentForum.MoveUp();
+        }
+
+        var adjacentForumMoved = new ForumMoved
+        {
+            SortOrder = adjacentForum.SortOrder,
+            TargetId = adjacentForum.Id,
+            TargetType = nameof(Forum),
+            SiteId = command.SiteId,
+            UserId = command.UserId
+        };
+
+        _dbContext.Events.Add(adjacentForumMoved.ToDbEntity());
+
+        await _dbContext.SaveChangesAsync();
+
+        _cacheManager.Remove(CacheKeys.Categories(command.SiteId));
+
+        return new Success(new IEvent[] { @event });
     }
 }
--- tests/Atles.Tests.Unit/Commands/Forums/UpdateForumHandlerTests.cs
+++ tests/Atles.Tests.Unit/Commands/Forums/UpdateForumHandlerTests.cs
@@ -4,63 +4,52 @@ using Atles.Data;
 using Atles.Data.Caching;
 using Atles.Domain;
 using AutoFixture;
-using FluentValidation;
-using FluentValidation.Results;
 using Microsoft.EntityFrameworkCore;
 using Moq;
 using NUnit.Framework;
 
-namespace Atles.Tests.Unit.Commands.Forums
+namespace Atles.Tests.Unit.Commands.Forums;
+
+[TestFixture]
+public class UpdateForumHandlerTests : TestFixtureBase
 {
-    [TestFixture]
-    public class UpdateForumHandlerTests : TestFixtureBase
+    [Test]
+    public async Task Should_update_forum_and_add_event()
     {
-        [Test]
-        public async Task Should_update_forum_and_add_event()
+        var options = Shared.CreateContextOptions();
+
+        var categoryId = Guid.NewGuid();
+        var siteId = Guid.NewGuid();
+
+        var category = new Category(categoryId, siteId, "Category", 1, Guid.NewGuid());
+        var forum = new Forum(categoryId, "Forum 1", "my-forum", "Forum 1", 1);
+
+        await using (var dbContext = new AtlesDbContext(options))
         {
-            var options = Shared.CreateContextOptions();
+            dbContext.Categories.Add(category);
+            dbContext.Forums.Add(forum);
+            await dbContext.SaveChangesAsync();
+        }
 
-            var categoryId = Guid.NewGuid();
-            var siteId = Guid.NewGuid();
+        await using (var dbContext = new AtlesDbContext(options))
+        {
+            var command = Fixture.Build<UpdateForum>()
+                .With(x => x.ForumId, forum.Id)
+                .With(x => x.CategoryId, forum.CategoryId)
+                .With(x => x.SiteId, siteId)
+                .Create();
 
-            var category = new Category(categoryId, siteId, "Category", 1, Guid.NewGuid());
-            var forum = new Forum(categoryId, "Forum 1", "my-forum", "Forum 1", 1);
+            var cacheManager = new Mock<ICacheManager>();
 
-            using (var dbContext = new AtlesDbContext(options))
-            {
-                dbContext.Categories.Add(category);
-                dbContext.Forums.Add(forum);
-                await dbContext.SaveChangesAsync();
-            }
+            var sut = new UpdateForumHandler(dbContext, cacheManager.Object);
 
-            using (var dbContext = new AtlesDbContext(options))
-            {
-                var command = Fixture.Build<UpdateForum>()
-                        .With(x => x.ForumId, forum.Id)
-                        .With(x => x.CategoryId, forum.CategoryId)
-                        .With(x => x.SiteId, siteId)
-                    .Create();
+            await sut.Handle(command);
 
-                var cacheManager = new Mock<ICacheManager>();
+            var updatedForum = await dbContext.Forums.FirstOrDefaultAsync(x => x.Id == command.ForumId);
+            var @event = await dbContext.Events.FirstOrDefaultAsync(x => x.TargetId == command.ForumId);
 
-                var validator = new Mock<IValidator<UpdateForum>>();
-                validator
-                    .Setup(x => x.ValidateAsync(command, new CancellationToken()))
-                    .ReturnsAsync(new ValidationResult());
-
-                var sut = new UpdateForumHandler(dbContext,
-                    cacheManager.Object,
-                    validator.Object);
-
-                await sut.Handle(command);
-
-                var updatedForum = await dbContext.Forums.FirstOrDefaultAsync(x => x.Id == command.ForumId);
-                var @event = await dbContext.Events.FirstOrDefaultAsync(x => x.TargetId == command.ForumId);
-
-                validator.Verify(x => x.ValidateAsync(command, new CancellationToken()));
-                Assert.AreEqual(command.Name, updatedForum.Name);
-                Assert.NotNull(@event);
-            }
+            Assert.AreEqual(command.Name, updatedForum.Name);
+            Assert.NotNull(@event);
         }
     }
 }
